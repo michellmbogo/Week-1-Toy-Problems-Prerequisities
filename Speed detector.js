1f3fa84c158ee6d5c2b5ea05b8c6dd6bdd914411
Michell Wambui Mbogo <mishie254@gmail.com>
2024-12-02 08:17:53 +0600

    readme

---
+++ Speed detector.js
@@ -0,0 +1,19 @@
+function checkSpeed(speed) {
+    const speedLimit = 70; // Speed limit in km/s
+    const kmPerPoint = 5;  // Kilometers per demerit point
+
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);// Calculate the demerit points
+        if (points > 12) {
+            console.log("License suspended");
+        } else {
+            console.log(`Points: ${points}`);
+        }
+    }
+}
+
+checkSpeed(180)
+
+
